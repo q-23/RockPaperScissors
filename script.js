89afbbf3403eac2c3938b6c8ce7c33c9dada3d22
Aleksander Kwiatkowski <aleksander.kwiatkowski@gmail.com>
2018-04-10 21:20:24 +0200

    Set initial game score

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ var pickRock = document.getElementById('js-playerPick_rock'),
 pickRock.addEventListener('click', function() { playerPick('rock') });
 pickPaper.addEventListener('click', function() { playerPick('paper') });
 pickScissors.addEventListener('click', function() { playerPick('scissors') });
+//set initial score
+
+var gameState = 'notStarted', //started //ended
+	player = {
+		name: '',
+		score: 0
+	},
+	computer = {
+		score: 0
+	};
